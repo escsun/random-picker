f4ba8b48d52e6e827378150ef51b1ffbbe4b55d7
escsun <escsunarcher@gmail.com>
2018-04-13 21:57:07 +0300

    add goodgame chat

---
+++ chat/goodgame.py
@@ -0,0 +1,35 @@
+import asyncio
+import websockets
+import json
+
+from api.goodgame import get_channel_id
+from utils.chat import websocket_chat_message
+
+
+async def goodgame_client_chat(channel):
+    """Клиент чат на websocket"""
+    channel_id = get_channel_id(channel)
+    async with websockets.connect('wss://chat-2.goodgame.ru/chat/websocket') as websocket:
+        # https://github.com/GoodGame/API/blob/master/Chat/protocol.md
+        # Заходим в чат по channel_id
+        await websocket.send(json.dumps({"type": "join", "data": {"channel_id": channel_id, "hidden": False}}))
+        while True:
+            try:
+                # Получаем сообщения
+                raw_msg = await websocket.recv()
+                if raw_msg is None:
+                    break
+                # Декодируем raw_msg
+                message = json.loads(raw_msg)
+                data = message['data']
+                # Если type сообщения message
+                if message['type'] == 'message':
+                    username = data['user_name']
+                    text = data['text']
+                    await websocket_chat_message("goodgame - {name}: {message}".format(
+                        name=username,
+                        message=text
+                    ))
+            except websockets.exceptions.ConnectionClosed:
+                # Переподключение к чату
+                asyncio.sleep(0, loop=await goodgame_client_chat(channel))
