c41a48198bf8ce5ae46d9f1a7953cad5dc07b45b
escsun <escsunarcher@gmail.com>
2018-04-13 22:18:10 +0300

    add main

---
+++ main.py
@@ -0,0 +1,18 @@
+import asyncio
+from tornado.platform.asyncio import AsyncIOMainLoop
+
+from server.app import server_load_app
+from tasks.app import tasks_load_app
+
+
+if __name__ == '__main__':
+    # Создаем мост между tornado и asyncio
+    AsyncIOMainLoop().install()
+    # Запускаем tornado ws сервер
+    server_app = server_load_app()
+    server_app.listen(port=8888)
+    # Запускаем задачи
+    tasks_app = tasks_load_app()
+
+    loop = asyncio.get_event_loop()
+    loop.run_forever()
