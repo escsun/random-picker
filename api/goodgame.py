b279343a80be48464fa87a8ff5292f05442659de
escsun <escsunarcher@gmail.com>
2018-04-13 21:55:06 +0300

    add goodgame api

---
+++ api/goodgame.py
@@ -0,0 +1,20 @@
+import requests
+import untangle
+
+
+def get_channel_id(channel):
+    try:
+        # Делаем запрос по имени канала и получаем xml
+        response = requests.get('https://goodgame.ru/api/getggchannelstatus?id={channel_name}'.format(
+            channel_name=channel))
+        try:
+            # Парсим xml ответ response.text
+            xml = untangle.parse(response.text)
+            # root > stream > id
+            channel_id = xml.root.stream['id']
+            # Возвращаем channel_id
+            return channel_id
+        except AttributeError as err:
+            print("Oops attribute error: ", err)
+    except requests.exceptions.RequestException as err:
+        print("Oops request error occurred: ", err)
