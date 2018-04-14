ba885095894430518858ad974f552bd077c5cc34
escsun <escsunarcher@gmail.com>
2018-04-14 13:37:46 +0300

    delete config path replace by load options

--- definitions.py
+++ definitions.py
@@ -2,8 +2,7 @@ import os
 import yaml
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is the project root path
-CONFIG_PATH = os.path.join(ROOT_DIR, 'configuration.yml')  # This is the configuration file
-LOAD_OPTIONS = yaml.safe_load(open(CONFIG_PATH))  # load options
+LOAD_OPTIONS = yaml.safe_load(open(os.path.join(ROOT_DIR, 'configuration.yml')))  # load options
 
 YOUTUBE_OPTIONS = LOAD_OPTIONS["youtube"]
 TWITCH_OPTIONS = LOAD_OPTIONS["twitch"]
