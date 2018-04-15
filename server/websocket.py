c7d32267362be860f3b202e18a0d27d57e6d30db
escsun <escsunarcher@gmail.com>
2018-04-16 02:01:41 +0300

    update websocket server

--- server/websocket.py
+++ server/websocket.py
@@ -1,12 +1,23 @@
 from tornado.websocket import WebSocketHandler
 
+active_clients = set()
+
 
 class EchoWebSocket(WebSocketHandler):
+
+    def open(self):
+        active_clients.add(self)
+
+    def check_origin(self, origin):
+        return True
 
     def data_received(self, chunk):
         pass
 
     def on_message(self, message):
-        # TODO delete print message latter
-        print(message)
-        self.write_message(message)
+        for client in active_clients:
+            client.write_message(message)
+
+    def on_close(self):
+        active_clients.remove(self)
+
