031e6e655b85c50e3293a8bb86d18f11fc9cc7a0
escsun <escsunarcher@gmail.com>
2018-04-16 02:41:35 +0300

    add message_to_send

--- utils/chat.py
+++ utils/chat.py
@@ -2,7 +2,12 @@ import websockets
 
 
 async def websocket_chat_message(message, ws='ws', address='localhost', port=8888):
-    """ Используется для посылки сообщения в websocket"""
+    """Используется для посылки сообщения в websocket"""
     uri = '{ws}://{address}:{port}'.format(ws=ws, address=address, port=port)
     async with websockets.connect(uri) as websocket:
         await websocket.send(message)
+
+
+def message_to_send(username, message, icon="default"):
+    """Форматированный вывод сообщения"""
+    return "{icon},{username},{message}".format(icon=icon, username=username, message=message)
